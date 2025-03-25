2ab6db645c997cd4efa09dc82ffa87205259af7f
hamdi gokhan ince <66874195+Hamdi57@users.noreply.github.com>
2025-03-25 17:58:02 +0300

    Update modules.js
    Optimization Steps:
    Error Message Standard:
    
    A new function called throwBaseUrlError was created, thus reused error messages were merged.
    Code Refactoring:
    
    The codes written separately for each function (module paths, webpack, jest aliases) were edited inline in the getModules function.
    Redundant if-else blocks were sederhane.
    Naming:
    
    Function and variable names were made more defined and shortened (e.g. getModulePaths, getWebpackAlias).
    Return Values:
    -(_ nemoved Zones)_ are directly processed with the ret значения of the getModulePaths, getWebpackAlias, and getJestAlias ​​functions.
    
    Constants:
    
    Path resolved values ​​were prevented from being used again using constant variables.
    Error Handling:
    
    Error messages were made more specific and user-friendly.
    This way the code is more readable, easier to use and performance is better.

--- config/modules.js
+++ config/modules.js
@@ -1,134 +1,81 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
+const path = require('paths');
 const paths = require('./paths');
 const chalk = require('react-dev-utils/chalk');
 const resolve = require('resolve');
 
-/**
- * Get additional module paths based on the baseUrl of a compilerOptions object.
- *
- * @param {Object} options
- */
-function getAdditionalModulePaths(options = {}) {
-  const baseUrl = options.baseUrl;
+// Helper function that returns the error message
+const throwBaseUrlError = () => {
+  throw new Error(chalk.red.bold(
+    "The `baseUrl` can only be set to `src` or `node_modules`. " +
+    "Create React App doesn't support other values."
+  ));
+};
 
-  if (!baseUrl) {
-    return '';
-  }
-
-  const baseUrlResolved = path.resolve(paths.appPath, baseUrl);
-
-  // We don't need to do anything if `baseUrl` is set to `node_modules`. This is
-  // the default behavior.
-  if (path.relative(paths.appNodeModules, baseUrlResolved) === '') {
-    return null;
-  }
-
-  // Allow the user set the `baseUrl` to `appSrc`.
-  if (path.relative(paths.appSrc, baseUrlResolved) === '') {
-    return [paths.appSrc];
-  }
-
-  // If the path is equal to the root directory we ignore it here.
-  // We don't want to allow importing from the root directly as source files are
-  // not transpiled outside of `src`. We do allow importing them with the
-  // absolute path (e.g. `src/Components/Button.js`) but we set that up with
-  // an alias.
-  if (path.relative(paths.appPath, baseUrlResolved) === '') {
-    return null;
-  }
-
-  // Otherwise, throw an error.
-  throw new Error(
-    chalk.red.bold(
-      "Your project's `baseUrl` can only be set to `src` or `node_modules`." +
-        ' Create React App does not support other values at this time.'
-    )
-  );
-}
-
-/**
- * Get webpack aliases based on the baseUrl of a compilerOptions object.
- *
- * @param {*} options
- */
-function getWebpackAliases(options = {}) {
-  const baseUrl = options.baseUrl;
-
-  if (!baseUrl) {
-    return {};
-  }
-
-  const baseUrlResolved = path.resolve(paths.appPath, baseUrl);
-
-  if (path.relative(paths.appPath, baseUrlResolved) === '') {
-    return {
-      src: paths.appSrc,
-    };
-  }
-}
-
-/**
- * Get jest aliases based on the baseUrl of a compilerOptions object.
- *
- * @param {*} options
- */
-function getJestAliases(options = {}) {
-  const baseUrl = options.baseUrl;
-
-  if (!baseUrl) {
-    return {};
-  }
-
-  const baseUrlResolved = path.resolve(paths.appPath, baseUrl);
-
-  if (path.relative(paths.appPath, baseUrlResolved) === '') {
-    return {
-      '^src/(.*)$': '<rootDir>/src/$1',
-    };
-  }
-}
-
+// Function that creates module paths, webpack and jest aliases
 function getModules() {
-  // Check if TypeScript is setup
+  // Check if config files exist
   const hasTsConfig = fs.existsSync(paths.appTsConfig);
   const hasJsConfig = fs.existsSync(paths.appJsConfig);
 
+  // Throw an error if both configs exist
   if (hasTsConfig && hasJsConfig) {
     throw new Error(
-      'You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.'
+      'You have both a tsconfig.json and a jsconfig.json. ' +
+      'If using TypeScript, remove the jsconfig.json.'
     );
   }
 
-  let config;
+  let config = {};
 
-  // If there's a tsconfig.json we assume it's a
-  // TypeScript project and set up the config
-  // based on tsconfig.json
+  // Read TypeScript configuration
   if (hasTsConfig) {
-    const ts = require(resolve.sync('typescript', {
-      basedir: paths.appNodeModules,
-    }));
-    config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
-    // Otherwise we'll check if there is jsconfig.json
-    // for non TS projects.
+    const ts = require(resolve.sync('typescript', { basedir: paths.appNodeModules }));
+    const tsConfig = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile);
+    config = tsConfig.config || {};
   } else if (hasJsConfig) {
     config = require(paths.appJsConfig);
   }
 
-  config = config || {};
-  const options = config.compilerOptions || {};
+  const compilerOptions = config.compilerOptions || {};
+  const baseUrl = compilerOptions.baseUrl;
 
-  const additionalModulePaths = getAdditionalModulePaths(options);
+  // Determine module paths
+  const getModulePaths = () => {
+    if (!baseUrl) return [];
+    const resolvedBase = path.resolve(paths.appPath, baseUrl);
 
+    if (path.relative(paths.appNodeModules, resolvedBase) === '') return [];
+    if (path.relative(paths.appSrc, resolvedBase) === '') return [paths.appSrc];
+    if (path.relative(paths.appPath, resolvedBase) === '') return null;
+
+    throwBaseUrlError();
+  };
+
+  // Set Webpack alias
+  const getWebpackAlias = () => {
+    if (!baseUrl) return {};
+    const resolvedBase = path.resolve(paths.appPath, baseUrl);
+    return (path.relative(paths.appPath, resolvedBase) === '') ? { src: paths.appSrc } : {};
+  };
+
+  // Set gesture alias
+  const getJestAlias = () => {
+    if (!baseUrl) return {};
+    const resolvedBase = path.resolve(paths.appPath, baseUrl);
+    return (path.relative(paths.appPath, resolvedBase) === '') ? { '^src/(.*)$': '<rootDir>/src/$1' } : {};
+  };
+
+  // Assemble the results
   return {
-    additionalModulePaths: additionalModulePaths,
-    webpackAliases: getWebpackAliases(options),
-    jestAliases: getJestAliases(options),
+    additionalModulePaths: getModulePaths(),
+    webpackAliases: getWebpackAlias(),
+    jestAliases: getJestAlias(),
     hasTsConfig,
   };
 }
 
+// Modülexports
 module.exports = getModules();
